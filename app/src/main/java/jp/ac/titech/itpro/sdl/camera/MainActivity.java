f4d6debbd0d232f44cab5d758fe701fc31e5fd90
maninthedark <1269651746@qq.com>
2020-05-26 17:10:32 +0800

    Use Camera

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -3,20 +3,31 @@ package jp.ac.titech.itpro.sdl.camera;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
     private Bitmap photoImage = null;
+    private String photoPath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +38,39 @@ public class MainActivity extends AppCompatActivity {
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 // TODO: You should setup appropriate parameters for the intent
 
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
-                    startActivityForResult(intent, REQ_PHOTO);
+                    File photoFile=null;
+                    try {
+                        photoFile=createPhotoFile();
+                    }
+                    catch (IOException ex) {
+                        Toast.makeText(MainActivity.this, "Cannot create photo file",Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    if(photoFile != null) {
+                        Uri photoUri= FileProvider.getUriForFile(MainActivity.this,"jp.ac.titech.itpro.sdl.fileprovider",photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+                        startActivityForResult(intent, REQ_PHOTO);
+                }
                 } else {
                     Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    private File createPhotoFile() throws IOException {
+        String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+        String photoFileName=""+timeStamp;
+        File storageDir=getExternalFilesDir((Environment.DIRECTORY_DCIM));
+        File photo=File.createTempFile(photoFileName,".jpg",storageDir);
+        photoPath=photo.getAbsolutePath();
+        return photo;
     }
 
     private void showPhoto() {
@@ -55,6 +87,14 @@ public class MainActivity extends AppCompatActivity {
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
                 // TODO: You should implement the code that retrieve a bitmap image
+                File photo=new File(photoPath);
+                Uri photoUri=Uri.fromFile(photo);
+                try {
+                    photoImage=MediaStore.Images.Media.getBitmap(getContentResolver(),photoUri);
+                }
+                catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
